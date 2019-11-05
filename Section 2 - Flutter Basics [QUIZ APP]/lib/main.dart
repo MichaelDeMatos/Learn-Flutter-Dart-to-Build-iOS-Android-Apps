ebfcee15c897ad30fadedceba526f3aba0cd40b9
Michael <michael@ocidental.com.br>
2019-11-05 14:27:16 -0300

    Stateless vs Stateful

--- Section 2 - Flutter Basics [QUIZ APP]/lib/main.dart
+++ Section 2 - Flutter Basics [QUIZ APP]/lib/main.dart
@@ -2,16 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var indexPergunta = 0;
+  void respondePergunta() {
+    setState(() {
+      indexPergunta += 1;
+    });
+    print(indexPergunta);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var perguntas = ['Qual sua cor favorita?', 'Qual seu animal favorito?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Michael'),
+          title: Text('Quizz'),
           centerTitle: true,
         ),
-        body: Text('Este é meu primeiro app!'),
+        body: Column(
+          children: [
+            Text(perguntas[indexPergunta]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: respondePergunta,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: () => print('Resposta 2'),
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: respondePergunta,
+            ),
+          ],
+        ),
       ),
     );
   }
